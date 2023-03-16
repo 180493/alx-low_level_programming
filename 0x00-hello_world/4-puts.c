3ac6a96a063653a52d1632814cc45c21da6bde35
180493 <motswabangweonica890@gmail.com>
2023-03-16 16:48:19 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -Prints"Programming is like building a multiligual
+ * puzzel,followed by a newline"
+ *
+ * Return: Always 0 
+ */
+int main(void)
+{
+    puts("/" Programming is like building a multilingual puzzel");
+
+    return (0);
+}
