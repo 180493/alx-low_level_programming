d756dbbb5b43b9a64a2dc9e15bd6d674841d1bb4
180493 <motswabangweonica890@gmail.com>
2023-03-16 17:29:41 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  * main -Prints"Programming is like building a multiligual
  * puzzel,followed by a newline"
@@ -8,7 +9,7 @@
  */
 int main(void)
 {
-    puts("/" Programming is like building a multilingual puzzel");
+    puts("/" Programming is like building a multilingual puzzel\n");
 
     return (0);
 }
