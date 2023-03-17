84573c88029d13dbd30948295383004b4e9fa50d
180493 <motswabangweonica890@gmail.com>
2023-03-17 02:04:23 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints exactly and that piece of art is useful.
+ *
+ * Return: Always 1 if Success
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful"\ Dorka korpa, 2015-10-19\n",59);
+retur (1);
+}
