7eb0808d608e7c44e4beea6be99b6ff1afbfc6b4
180493 <motswabangweonica890@gmail.com>
2023-03-19 04:02:27 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -6,8 +7,6 @@
  * Return: Always 1.
  */
 int main(void)
-
-
 {
 	write(2, "and that piece of art is useful\" - Dora korpa, 2015-10-19\n", 59);
         return (1);
