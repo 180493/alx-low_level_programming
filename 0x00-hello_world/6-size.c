736c36bf9c6eda4ab20807c46720134750d289db
180493 <motswabangweonica890@gmail.com>
2023-03-19 10:11:25 -0700

    101-quote.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Prints the size of various typesa based on
- * the computer it is compiled and run on..
+ * main - Prints "the size of various typesa based on
+ * the computer it is compiled and run on.."
  * Return: Always 0.
  */
 
 int main(void)
-
 {
 	printf(Size of a char: %zu byte(s)\n", sizeof(char));
 
