3c3173f6359a14276b2e364f94376b33624fc482
180493 <motswabangweonica890@gmail.com>
2023-03-20 00:57:09 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Prints "Programming is like building a multilingual 
- * puzzel,followed by a newline."
+ * puzzle,followed by a newline."
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multiligual puzzel");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
