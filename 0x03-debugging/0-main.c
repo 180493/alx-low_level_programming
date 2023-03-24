fcafb001310f990d18de108d660b3a7e4435a936
180493 <motswabangweonica890@gmail.com>
2023-03-24 03:56:30 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - tests function that prints is positive or negative
+ * main - Test function for  positive or negative
  * Return: 0
  */
 
 int main(voi)
 {
-
 	int i;
 
 	i =0;
 	positive_or_negative(i);
-	return (0);
+
+return (0);
 }
