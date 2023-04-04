c448d17e5d9154168b194ad65736f22b77882965
180493 <motswabangweonica890@gmail.com>
2023-04-04 14:15:31 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 int _putchar(char c);
 
-void reset_to_98(int*n);
+void reset_to_98(int *n );
 
 void swap_int(int *a, int *b);
 
@@ -20,4 +20,4 @@ void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
 
-int _atoil(char *s);
+int _atoil(char );
